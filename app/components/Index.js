ea8e02ac7158eecd17047628fa9843c8b40e6ba4
michaeledwards91 <michael.edwards1991@gmail.com>
2017-01-22 06:34:28 -0600

    Added background color

--- app/components/Index.js
+++ app/components/Index.js
@@ -11,11 +11,11 @@ var Index = React.createClass({
 				{/*Navbar*/}
 				<nav>
 			    <div className="nav-wrapper">
-			      <a href="#" className="brand-logo">Logo</a>
+			      <a href="#" className="brand-logo">Michael Edwards</a>
 			      <ul id="nav-mobile" className="right hide-on-med-and-down">
-			        <li><a href="sass.html">Sass</a></li>
-			        <li><a href="badges.html">Components</a></li>
-			        <li><a href="collapsible.html">JavaScript</a></li>
+			        <li><a href="/#">Home</a></li>
+			        <li><a href="/#portfolio">Work</a></li>
+			        <li><a href="/#about">About</a></li>
 			      </ul>
 			    </div>
 			  </nav>
