b056e44784d3c8a5b27be88f59486b9f1c6fba1a
michaeledwards91 <michael.edwards1991@gmail.com>
2017-01-23 08:45:56 -0600

    Hard coded navbar into index.html to try to resolve issue with styles not applying

--- app/components/Index.js
+++ app/components/Index.js
@@ -11,7 +11,7 @@ var Index = React.createClass({
 		return (
 			<div className="indexContainer">
 				
-				<Navbar />
+				{/* <Navbar /> */}
 				<div className="indexContent">
 					<h1 className="center" id="indexH1">Michael Edwards</h1>
 					<hr></hr>
--- app/components/Portfolio.js
+++ app/components/Portfolio.js
@@ -10,26 +10,26 @@ var Portfolio = React.createClass({
 	render: function() {
 		return (
 			<div className="portfolioContainer">
-				<Navbar />
+				{/* <Navbar /> */}
 
 				<div className="portfolioContent">
 					<div className="container">
 						<div className="row">
 
 								<div className="col s12 m5">
-									  <div className="card sticky-action hoverable">
-									    <div className="card-image waves-effect waves-block waves-light">
-									      <img className="activator" src="./images/yumyum.png"></img>
-									    </div>
-									    <div className="card-content">
-									      <span className="card-title activator grey-text text-darken-4">Card Title<i className="material-icons right">more_vert</i></span>
-									      <p><a href="#">This is a link</a></p>
-									    </div>
-									    <div className="card-reveal">
-									      <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
-									      <p>Here is some more information about this product that is only revealed once clicked on.</p>
-									    </div>
-									  </div>
+								  <div className="card sticky-action hoverable">
+								    <div className="card-image waves-effect waves-block waves-light">
+								      <img className="activator" src="./images/yumyum.png"></img>
+								    </div>
+								    <div className="card-action">
+								      <span className="card-title activator grey-text text-darken-4">Card Title<i className="material-icons right">more_vert</i></span>
+								      <p><a href="#">This is a link</a></p>
+								    </div>
+								    <div className="card-reveal">
+								      <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
+								      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+								    </div>
+								  </div>
 								</div>
 
 								<div className="col s12 m2">
@@ -37,19 +37,19 @@ var Portfolio = React.createClass({
 								</div>
 
 								<div className="col s12 m5">
-									  <div className="card sticky-action hoverable">
-									    <div className="card-image waves-effect waves-block waves-light">
-									      <img className="activator" src="./images/yumyum.png"></img>
-									    </div>
-									    <div className="card-content">
-									      <span className="card-title activator grey-text text-darken-4">Card Title<i className="material-icons right">more_vert</i></span>
-									      <p><a href="#">This is a link</a></p>
-									    </div>
-									    <div className="card-reveal">
-									      <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
-									      <p>Here is some more information about this product that is only revealed once clicked on.</p>
-									    </div>
-									  </div>
+								  <div className="card sticky-action hoverable">
+								    <div className="card-image waves-effect waves-block waves-light">
+								      <img className="activator" src="./images/yumyum.png"></img>
+								    </div>
+								    <div className="card-action">
+								      <span className="card-title activator grey-text text-darken-4">Card Title<i className="material-icons right">more_vert</i></span>
+								      <p><a href="#">This is a link</a></p>
+								    </div>
+								    <div className="card-reveal">
+								      <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
+								      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+								    </div>
+								  </div>
 								</div>
 
 						</div>
