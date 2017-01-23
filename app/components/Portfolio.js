744786c2b19623a03a361fdb78f99e8de7db865a
michaeledwards91 <michael.edwards1991@gmail.com>
2017-01-22 22:57:24 -0600

    Added yumyum image and changed portfolio grid layout

--- app/components/Portfolio.js
+++ app/components/Portfolio.js
@@ -15,21 +15,41 @@ var Portfolio = React.createClass({
 				<div className="portfolioContent">
 					<div className="container">
 						<div className="row">
-							
-								<div className="col s1 m1">
+
+								<div className="col s12 m5">
+									  <div className="card sticky-action hoverable">
+									    <div className="card-image waves-effect waves-block waves-light">
+									      <img className="activator" src="./images/yumyum.png"></img>
+									    </div>
+									    <div className="card-content">
+									      <span className="card-title activator grey-text text-darken-4">Card Title<i className="material-icons right">more_vert</i></span>
+									      <p><a href="#">This is a link</a></p>
+									    </div>
+									    <div className="card-reveal">
+									      <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
+									      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+									    </div>
+									  </div>
+								</div>
+
+								<div className="col s12 m2">
 									{/*Empty Div*/}
 								</div>
 
-								<div className="col s10 m4">
-									<p>test</p>
-								</div>
-
-								<div className="col s1 m2">
-									{/*Empty Div*/}
-								</div>
-
-								<div className="col s10 m4">
-									<p>test</p>
+								<div className="col s12 m5">
+									  <div className="card sticky-action hoverable">
+									    <div className="card-image waves-effect waves-block waves-light">
+									      <img className="activator" src="./images/yumyum.png"></img>
+									    </div>
+									    <div className="card-content">
+									      <span className="card-title activator grey-text text-darken-4">Card Title<i className="material-icons right">more_vert</i></span>
+									      <p><a href="#">This is a link</a></p>
+									    </div>
+									    <div className="card-reveal">
+									      <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
+									      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+									    </div>
+									  </div>
 								</div>
 
 						</div>
