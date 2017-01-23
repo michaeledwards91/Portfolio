920754bdf9903855ead3f7532358b0339708f67f
michaeledwards91 <michael.edwards1991@gmail.com>
2017-01-23 15:10:11 -0600

    Card styling

--- app/components/Portfolio.js
+++ app/components/Portfolio.js
@@ -16,30 +16,28 @@ var Portfolio = React.createClass({
 					<div className="container">
 						<div className="row">
 
-								<div className="col s12 m5">
+								<div className="col s12 m6">
 								  <div className="card sticky-action hoverable">
 								    <div className="card-image waves-effect waves-block waves-light">
-								      <img className="activator" src="./images/yumyum.png"></img>
+								      <img className="activator responsive-img" src="./images/yumyum3.png"></img>
+
 								    </div>
+								    
 								    <div className="card-action">
-								      <span className="card-title activator grey-text text-darken-4">Card Title<i className="material-icons right">more_vert</i></span>
-								      <p><a href="#">This is a link</a></p>
+								      <div className="card-title activator grey-text text-darken-4"><a href="http://yum-yumrecipes.herokuapp.com/" target="_blank">Yum-Yum Recipes</a><i className="material-icons right">more_vert</i></div>
+								      <p><a className="githubLink" href="#">This is a link</a></p>
 								    </div>
 								    <div className="card-reveal">
-								      <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
-								      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+								      <span className="card-title grey-text text-darken-4">Yum-Yum Recipes<i className="material-icons right">close</i></span>
+								      <p>MySQL, Node, Express, and Handlebars application that displays recipes, ingredients, and instructions to the user and allows the user to save ingredients to a shopping list. Used Trello and Git/GitHub to collaborate with 3 other students. Personally designed and implemented code to style page and display results from database queries on the page.</p>
 								    </div>
 								  </div>
 								</div>
-
-								<div className="col s12 m2">
-									{/*Empty Div*/}
-								</div>
-
-								<div className="col s12 m5">
+								
+								<div className="col s12 m6">
 								  <div className="card sticky-action hoverable">
 								    <div className="card-image waves-effect waves-block waves-light">
-								      <img className="activator" src="./images/yumyum.png"></img>
+								      <img className="activator responsive-img" src="./images/typsyweather.png"></img>
 								    </div>
 								    <div className="card-action">
 								      <span className="card-title activator grey-text text-darken-4">Card Title<i className="material-icons right">more_vert</i></span>
