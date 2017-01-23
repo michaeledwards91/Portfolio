6a52be4f35707dabaef83c6890ef61b927f27996
michaeledwards91 <michael.edwards1991@gmail.com>
2017-01-23 17:22:16 -0600

    Added form to contact page. Added route for contact form to email self

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -10,9 +10,36 @@ var Contact = React.createClass({
 	render: function() {
 		return (
 			<div className="contactContainer">
+				<div className="container">
+					<div className="row">
+						<div className="col s12 m8 push-m2">
+							
+							<form action="/api/mailer" method="POST">
+								<div className="input-field col s12">
+	                <input type="text" name="name" id="nameInput"></input>
+	                <label for="name">Your Name</label>
+	            	</div>
 
+	            	<div className="input-field col s12">
+	                <input type="text" name="email" id="emailInput"></input>
+	                <label for="email">Your Email Address</label>
+	            	</div>
+
+	            	<div className="input-field col s12">
+	                <textarea className="materialize-textarea" name="message" id="messageInput"></textarea>
+	                <label for="message">Your Message</label>
+            		</div>
+
+
+	            	<button className="btn waves-effect waves-light" type="submit" name="action">Submit
+		            <i className="material-icons right">send</i>
+		            </button>
+							</form>
+
+						</div>
+					</div>
+				</div>
 			</div>
-
 		);
 	}
 
--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 //Include dependencies
 var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 
 //Express instance
 var app = express();
 
 var PORT = process.env.PORT || 3000;
+
+//Body Parser setup
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+//Method Override setup
+app.use(methodOverride("_method"));
 
 //Express setup
 app.use(express.static("./public"));
@@ -14,8 +25,11 @@ app.get("/", function(req, res) {
 	res.sendFile(__dirname + "/public/index.html");
 });
 
-app.get("/portfolio", function(req, res) {
-	res.sendFile(__dirname + "/public/portfolio.html");
+//Route for node mailer
+app.post("/api/mailer", function(req, res) {
+	console.log(req.body);
+	
+	res.redirect("/#/contact");
 });
 
 //Listener
