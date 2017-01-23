d2afb504d18570b855b5a575bbae8efe5b30b905
michaeledwards91 <michael.edwards1991@gmail.com>
2017-01-22 22:23:34 -0600

    Set up portfolio page grid

--- app/components/Portfolio.js
+++ app/components/Portfolio.js
@@ -1,14 +1,41 @@
 //Include React
 var React = require("react");
 
+//Include child components
+var Navbar = require("./Navbar");
+
 //Create Portfolio Component
 var Portfolio = React.createClass({
 
 	render: function() {
 		return (
+			<div className="portfolioContainer">
+				<Navbar />
 
-			<h1>suh</h1>
+				<div className="portfolioContent">
+					<div className="container">
+						<div className="row">
+							
+								<div className="col s1 m1">
+									{/*Empty Div*/}
+								</div>
 
+								<div className="col s10 m4">
+									<p>test</p>
+								</div>
+
+								<div className="col s1 m2">
+									{/*Empty Div*/}
+								</div>
+
+								<div className="col s10 m4">
+									<p>test</p>
+								</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
 		);
 	}
 
