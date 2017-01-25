b5c7803ee17e46e9b3521fe646c48db1d47f756e
michaeledwards91 <michael.edwards1991@gmail.com>
2017-01-25 11:47:13 -0600

    Added skills icons

--- app/components/SkillsGroup.js
+++ app/components/SkillsGroup.js
@@ -6,8 +6,28 @@ var SkillsGroup = React.createClass({
 	render: function() {
 		return (
 
+				
 			<div className="skillsGroupContainer">
+				<h3 className="center">Skills</h3>
 
+				<div className="skillsIconsWrapper">
+					{/*HTML5, CSS3, JavaScript, jQuery, Node.js, Express.js, React.js, MongoDB, MySQL, Heroku, Materialize, Bootstrap, Git, Handlebars.js*/}
+					<img className="skillsIcon" src="./images/skill-icons/html5.ico"></img>
+					<img className="skillsIcon" src="./images/skill-icons/css3.png"></img>
+					<img className="skillsIcon" src="./images/skill-icons/javascript.png"></img>
+					<img className="skillsIcon" src="./images/skill-icons/jquery.png"></img>
+					<img className="skillsIcon" src="./images/skill-icons/nodejs.png"></img>
+					<img className="skillsIcon" src="./images/skill-icons/expressjs.png"></img>
+					<img className="skillsIcon" src="./images/skill-icons/reactjs.png"></img>
+					<img className="skillsIcon" src="./images/skill-icons/mongodb.png"></img>
+					<img className="skillsIcon" src="./images/skill-icons/mysql.png"></img>
+					<img className="skillsIcon" src="./images/skill-icons/heroku.png"></img>
+					<img className="skillsIcon" src="./images/skill-icons/materializecss.png"></img>
+					<img className="skillsIcon" src="./images/skill-icons/bootstrapcss.png"></img>
+					<img className="skillsIcon" src="./images/skill-icons/git.png"></img>
+					<img className="skillsIcon" src="./images/skill-icons/handlebars.png"></img>
+
+				</div>
 			</div>
 
 		);
