0d302a0bd57c71699db4538dccebcd50677b6141
michaeledwards91 <michael.edwards1991@gmail.com>
2017-02-16 04:04:28 -0600

    Roll in effect on main page and navbar name click

--- public/frontend.js
+++ public/frontend.js
@@ -11,10 +11,17 @@ $(document).ready(function() {
 		$("#messageInput").focus();
 	});
 
+	//Toast to notify user of successful email send
 	$(document).on("click", "#submitBtn", function() {
 		Materialize.toast("Thanks for contacting me!", 4000);
 	});
 
-	$("#indexH1").textillate( {in: { effect: 'rollIn' } } );
+	//Rotate In effect on page load
+	$("#indexH1").textillate( {in: { effect: 'rotateIn' }, out: { effect: 'hinge', shuffle: true }} );
+	//Rotate In effect when clicking name
+	$(document).on("click", ".brand-logo", function() {
+		$("#indexH1").textillate( {in: { effect: 'rotateIn' }, out: { effect: 'hinge', shuffle: true } } );
+		$("#indexH1").textillate("in");
+	});
 
 });
